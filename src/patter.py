09177a5e936a6dfaeee8f0e0d2394e86180f6036
aravindp98 <aravindkumar.p20@gmail.com>
2023-12-01 22:17:33 +0530

    sample  python script

--- src/patter.py
+++ src/patter.py
@@ -1,5 +1,14 @@
-def work():
-    print("hello world")
+class Pattern:
+    def pattern_printing(self):
 
-    
-work()
+        for i in range(1, self):
+            for j in range(i + 1):
+                print(" ", end=" ")
+            for j in range(i, self):
+                print("*", end=" ")
+
+            print("\r")
+
+
+c1 = Pattern
+c1.pattern_printing(6)
