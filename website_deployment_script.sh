76e1b73db591f8afb862317fd3776edb01edf368
aravindp98 <aravindkumar.p20@gmail.com>
2023-12-10 15:26:31 +0530

    website deployment script

---
+++ website_deployment_script.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "#############################################################"
+echo "##### Installing Packages and start the httpd service #####"
+echo "#################################################################"
+
+sudo yum install wget unzip git httpd -y
+
+systemctl start httpd
+systemctl enable httpd
+
+
+echo "#################################################################"
+echo "Downloading the website and unzip the webfiles"
+echo "#################################################################"
+
+wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip > /tmp/webfiles/
+cd /tmp/webfiles/
+unzip 2137_barista_cafe.zip
+cp -rf 2137_barista_cafe/* /var/www/html
+
+echo "#################################################################"
+echo "restart the httpd service"
+echo "#################################################################"
+
+systemctl restart httpd
+systemctl enable httpd
+
+echo "#################################################################"
+echo "delete the webfiles"
+echo "#################################################################"
+
+rm -rf /tmp/webfiles/*
+
+
