16880a149b1b5c8253770ad82cd1fea1784b5337
aravindp98 <aravindkumar.p20@gmail.com>
2024-02-06 06:55:28 +0530

    website deployment script

---
+++ bash_scripts/cs_ev_dm_mon.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+command_substitution() {
+    echo "################################"
+    echo "Command Substitution"
+    echo "################################"
+
+    MEMORY=$(free -m | grep Mem | awk '{print $4}')
+
+    echo "Free Memory: $MEMORY"
+    echo "Exit code: $?"
+
+    echo "$MEMORY"
+}
+
+export_variable() {
+    echo "################################"
+    echo "Export Variable"
+    echo "################################"
+
+    SEASON="spring"
+    echo "From next month $SEASON is going to start"
+}
+
+decision_making() {
+    echo "################################"
+    echo "Decision Making"
+    echo "################################"
+
+    MEMORY=$1
+
+    if [ $MEMORY -gt 1000 ]; then
+        echo "There is enough free memory."
+    else
+        echo "Low memory warning!"
+    fi
+}
+
+MEMORY=$(command_substitution)
+decision_making "$MEMORY"
