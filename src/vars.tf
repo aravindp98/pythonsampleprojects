656741e49dc8f2191b568ba8b20b39e5298087d3
aravindp98 <aravindkumar.p20@gmail.com>
2024-02-08 07:44:29 +0530

    feat:subnet is added

---
+++ src/vars.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "zone1" {
+  default = "us-east-1a"
+}
+
+variable "zone2" {
+  default = "us-east-1b"
+}
+
--- src/vpc.tf
+++ src/vpc.tf
@@ -6,3 +6,27 @@ resource "aws_vpc" "devOps_project" {
     Name = "devOps_project"
   }
 }
+
+resource "aws_subnet" "PrivateA" {
+  vpc_id = aws_vpc.devOps_project.id
+  cidr_block = "10.0.0.1/20"
+  availability_zone = var.zone1
+}
+
+resource "aws_subnet" "PrivateB" {
+  vpc_id = aws_vpc.devOps_project.id
+  cidr_block = "10.0.16.0/20"
+  availability_zone = var.zone2
+}
+
+resource "aws_subnet" "PublicA" {
+  vpc_id = aws_vpc.devOps_project.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = var.zone1
+}
+
+resource "aws_subnet" "PublicB" {
+  vpc_id = aws_vpc.devOps_project.id
+  cidr_block = "10.0.23.0/24"
+  availability_zone = var.zone2
+}
